a0cea178f87f6de7a09e5bb81dfb4617ca5f1d04
peter <peters.shaker@gmail.com>
2020-04-22 02:02:04 +0200

    main component

---
+++ src/components/Main.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Dnd from "./Dnd";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+const Main = () => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Dnd />
+    </div>
+  );
+};
+
+export default Main;
