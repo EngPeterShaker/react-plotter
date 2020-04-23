f12c86ca187d8faf4156fc47d0e5cd0e7eafe508
peter <peters.shaker@gmail.com>
2020-04-23 17:19:12 +0200

    redux actions

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -3,16 +3,14 @@ import { GET_DATA ,GET_COLOUMN_DATA } from "../actionTypes";
 
 export const getAllColumnsData =()=> dispatch =>{
   getAllAColoumns().then(res=> {
-    console.log('res', res)
     dispatch({type: GET_DATA , payload : res.data})
   })
 }
 
 
 
-export const getColoumnData =()=> dispatch =>{
-  getData().then(res=> {
-    console.log('res', res)
-    // dispatch
+export const getColoumnData =(body)=> dispatch =>{
+  getData(body).then(res=> {
+    dispatch({type: GET_COLOUMN_DATA , payload : res.data})
   })
 }
